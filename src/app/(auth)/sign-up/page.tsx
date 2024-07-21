28e966ffd4df20a99f527734080635b1c4afb0b4
Bendako <62848655+Bendako@users.noreply.github.com>
2024-07-21 21:10:44 +0300

    add toasts

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -10,6 +10,9 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { AuthCredentialsValidator, TAuthCredentialsValidator } from "@/lib/validators/account-credentials-validator"
 import { trpc } from "@/trpc/client"
+import { toast } from "sonner";
+import { ZodError } from "zod"
+import { useRouter } from "next/navigation"
 
 const Page = () => {
 
@@ -21,8 +24,31 @@ const Page = () => {
         resolver: zodResolver(AuthCredentialsValidator)
     })
 
-    const { mutate, isLoading } = trpc.auth.createPayloadUser.useMutation({
+    const router = useRouter()
 
+    const { mutate, isLoading } = trpc.auth.createPayloadUser.useMutation({
+        onError: (err) => {
+            if (err.data?.code === "CONFLICT") {
+                toast.error(
+                    "This email is already in use. Sign in instead?"
+                )
+
+                return
+            }
+
+            if (err instanceof ZodError) {
+                toast.error(err.issues[0].message)
+
+                return
+            }
+
+            toast.error("Something went wrong. Please try again.")
+        },
+
+        onSuccess: ({ sentToEmail }) => {
+            toast.success(`Verification email sent to ${sentToEmail}.`)
+            router.push("/verify-email?to=" + sentToEmail)
+        }
     })
 
     const onSubmit = ({ 
